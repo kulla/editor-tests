b1de94dc34844fc9058d799268fd2446b886df7a
Stephan Kulla <git.mail@kulla.me>
2022-01-16 15:34:24 +0100

    contenteditable: Add `onInput`

--- src/stories/fundamentals/contenteditable.tsx
+++ src/stories/fundamentals/contenteditable.tsx
@@ -1,11 +1,45 @@
-export function BasicContentEditableDiv({ children }: { children: string }) {
+import { useState, ComponentProps } from "react";
+
+export function InvestigateInputEvent() {
+  const [eventState, setEventState] = useState<InputEvent | null>(null);
+  const preText =
+    eventState !== null
+      ? JSON.stringify(
+          {
+            data: eventState.data,
+            inputType: eventState.inputType,
+          },
+          null,
+          2
+        )
+      : "null";
+
+  return (
+    <>
+      <BasicContentEditableDiv
+        onInput={(e) => {
+          console.log(e);
+          setEventState(e.nativeEvent as InputEvent);
+        }}
+      ></BasicContentEditableDiv>
+      <pre>lastInputEvent: {preText}</pre>
+    </>
+  );
+}
+
+export function BasicContentEditableDiv(
+  props: {
+    children?: string;
+  } & ComponentProps<"div">
+) {
   return (
     <div
       contentEditable
       suppressContentEditableWarning
       style={{ border: "1px solid black", padding: "1em" }}
+      {...props}
     >
-      {children}
+      {props.children ?? "I am an editable div"}
     </div>
   );
 }
